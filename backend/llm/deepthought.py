2db5a243e880346687ca4eef1a1d0f5dec55950c
synthMoza <eganian.aa@phystech.edu>
2024-04-07 15:55:19 +0300

    Implement DeepThought class and demoapp for testing

---
+++ backend/llm/deepthought.py
@@ -0,0 +1,6 @@
+from abc import ABC, abstractmethod
+
+class AbstractDeepThought(ABC):
+    @abstractmethod
+    def ask(self, input_question: str, document: str) -> str: 
+        pass
---
+++ backend/llm/gigathought.py
@@ -0,0 +1,18 @@
+from deepthought import AbstractDeepThought
+from langchain.chat_models.gigachat import GigaChat
+from langchain.prompts import load_prompt
+
+class GigaThought(AbstractDeepThought):
+    def __init__(self, credentials, config_file_path):
+        self.chat = GigaChat(credentials=credentials, verify_ssl_certs=False)
+        self.prompt = load_prompt(config_file_path)
+        self.chain = self.prompt | self.chat
+    def ask(self, input_question: str, document: str) -> str:
+        message = self.chain.invoke(
+            {
+                "input_question": input_question,
+                "context": document
+            }
+        )
+
+        return message.content
